ad3215a3aa9e6629f5b56e4ec38ad1102a437584
andreimanciu <79370729+andreimanciu@users.noreply.github.com>
2021-05-27 20:37:25 +0300

    Create B9.c

---
+++ Marire nota/L2/B9.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int a, b;
+//	printf("a=");
+//	scanf("%d", &a);
+//	printf("b=");
+//	scanf("%d", &b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d",a);
+//	printf("\nb=%d",b);
+//	system("pause");
+//	return 0;
+//}
