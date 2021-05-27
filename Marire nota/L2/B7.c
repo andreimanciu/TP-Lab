8f3759824e4e7a6d63b324b436b215e6e2c6fb81
andreimanciu <79370729+andreimanciu@users.noreply.github.com>
2021-05-27 20:32:47 +0300

    Create B7.c

---
+++ Marire nota/L2/B7.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int nr(int x)
+//{
+//	int s = 0,i;
+//	for (i = 7;i >= 0;i--)
+//		if (x >> i & 1)s++;
+//	return s;
+//}
+//int main()
+//{
+//	int i = 1, n, s = 0;
+//	printf("n=");
+//	scanf("%d", &n);
+//	while (i <= n)
+//	{
+//		s += nr(i);
+//		i++;
+//	}
+//	printf("%d", s);
+//	system("pause");
+//	return 0;
+//}
