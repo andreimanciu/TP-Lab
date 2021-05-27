ef9d10d14f9243282b22518dadc846ef6a7b83ea
andreimanciu <79370729+andreimanciu@users.noreply.github.com>
2021-05-27 20:27:35 +0300

    Create B6.c

---
+++ Marire nota/L2/B6.c
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//void afis(int x,int *c)
+//{
+//	int i;
+//	for (i = 7;i >= 0;i--)
+//	{
+//		if (x >> i & 1) 
+//		{
+//			printf("1 ");(*c)++;
+//		}
+//		else printf("0 ");
+//	}
+//}
+//int main()
+//{
+//	int a,i,c=0,max=0,t=7;
+//	printf("a=");
+//	scanf("%d", &a);
+//	afis(a,&c);
+//	for (i = 0;i < c;i++)
+//	{
+//		max =max + pow(2, t--);
+//	}
+//	printf("\nmax=%d ", max);
+//	for (i = 0; i <= 7;i++)
+//	{
+//		if (c != 0) 
+//		{
+//			printf("1 ");c--;
+//		}
+//		else printf("0 ");
+//	}
+//	printf(" -Valoare maxima");
+//	system("pause");
+//	return 0;
+//}
